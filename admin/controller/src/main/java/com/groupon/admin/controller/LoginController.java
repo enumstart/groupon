17bd0ca950485d8c0d5a143d2deaad1990ce86b5
wu <ndsywyc@users.noreply.github.com>
2018-01-31 22:40:01 +0800

    20180131

--- admin/controller/src/main/java/com/groupon/admin/controller/LoginController.java
+++ admin/controller/src/main/java/com/groupon/admin/controller/LoginController.java
@@ -4,4 +4,7 @@ package com.groupon.admin.controller;
  * Created by enum on 2018/1/31.
  */
 public class LoginController {
+    public static void main(String[] args) {
+        System.out.println("..dd..");
+    }
 }
